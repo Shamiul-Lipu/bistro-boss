7bbea44525b40588e3039dbae239fcd19fbb3253
unknown <shamiullipu@gmail.com>
2023-05-23 22:56:24 +0600

    react review added

--- bistro-boss-client/src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ bistro-boss-client/src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -23,6 +23,7 @@ const PopularMenu = () => {
                     menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
                 }
             </div>
+            <button className="btn btn-outline ">View Full Menu</button>
         </section>
     );
 };
